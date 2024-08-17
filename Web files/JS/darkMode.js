e901af7446cd9db55896d9fd1aa8403363efacc3
Hussein <shsmohmed4@gmail.com>
2024-08-17 22:29:16 +0300

    search and filter

--- Web files/JS/darkMode.js
+++ Web files/JS/darkMode.js
@@ -1,3 +1,20 @@
-/*
-    Hussein's Task
-*/
+// handel dark mode
+let btnDark = document.querySelector('.dark');
+let flagdark = true;
+btnDark.addEventListener('click', () => {
+    if (flagdark) {
+        document.documentElement.style.setProperty('--main-color', '#1f2d36');
+        document.documentElement.style.setProperty('--head-color', '#2b3743');
+        document.documentElement.style.setProperty('--text-color', '#fff');
+        btnDark.classList.remove('fa-moon');
+        btnDark.classList.add('fa-sun');
+        flagdark = false;
+    } else {
+        document.documentElement.style.setProperty('--main-color', '#e7e7e7');
+        document.documentElement.style.setProperty('--head-color', '#ffffff');
+        document.documentElement.style.setProperty('--text-color', '#000');
+        btnDark.classList.remove('fa-sun');
+        btnDark.classList.add('fa-moon');
+        flagdark = true;
+    }
+})
--- Web files/JS/main.js
+++ Web files/JS/main.js
@@ -1,21 +1,21 @@
 async function country_page(element, data, card) {
     let cssContent;
-        await fetch("../CSS/countryPage.css")
-            .then(data => data.text())
-            .then(async data => { 
-                cssContent = data; 
-            });
-
-    let jsContent;
-        await fetch("./countryPage.js")
+    await fetch("../CSS/countryPage.css")
         .then(data => data.text())
-        .then(async data => { 
-            jsContent = data; 
+        .then(async data => {
+            cssContent = data;
         });
+
+    // let jsContent;
+    // await fetch("./countryPage.js")
+    //     .then(data => data.text())
+    //     .then(async data => {
+    //         jsContent = data;
+    //     });
 
     const name = element.name.replace(/\s*\(.*?\)\s*/g, '');
     const name_without_spacing = name.replace(/\s/g, '-').replace(/\s*\(.*?\)\s*/g, '').replace(/\,/g, '').replace(/\'/g, '');
-    console.log(name_without_spacing);
+    // console.log(name_without_spacing);
     const nativeName = element.nativeName;
     const population = element.population;
     const region = element.region;
@@ -27,13 +27,20 @@ async function country_page(element, data, card) {
     const borders = element.borders;
     const flag_path = element.flags.png;
 
-    borders.map(ele => data.find(e => e.alpha3Code === ele).name);
-    const borders_list = document.createElement('div');
-    borders_list.classList.add('borders');
-    for(border of borders){
-        const item = document.createElement('span');
-        item.innerText = border;
-        borders_list.appendChild(item);
+    let borders_list;
+    if (borders !== undefined) {
+        borders.map(ele => data.find(e => e.alpha3Code === ele).name);
+        borders_list = document.createElement('div');
+        borders_list.classList.add('borders');
+        for (border of borders) {
+            const item = document.createElement('span');
+            item.innerText = border;
+            borders_list.appendChild(item);
+        }
+    } else {
+        borders_list = document.createElement('div');
+        borders_list.classList.add('borders');
+        borders_list.innerText = "";
     }
 
     card.addEventListener('click', () => {
@@ -101,14 +108,59 @@ async function country_page(element, data, card) {
         a.click();
     });
 }
-
-
 const cards_section = document.getElementById('cards-section');
 
-if (cards_section === null){
+if (cards_section === null) {
     console.error("cards_section is null");
 }
-
+// search functionality
+let selectedRegion = document.querySelector('.filter-select');
+selectedRegion.value = 'All';
+let searchinput = document.querySelector('.search input');
+let searchbtn = document.querySelector('.search-btn');
+let deleteSearch = document.querySelector('.delete-search');
+async function search(data, countryTosearch) {
+    for (let i = 0; i < data.length; i++) {
+        if (countryTosearch.length > data[i].name.length) {
+            let coun = data[i].name.replace(/[^a-zA-Z]/g, '').toLowerCase();
+            let hidecard = document.querySelector(`#${coun}`);
+            if (hidecard) {
+                hidecard.style.display = 'none';
+            }
+        } else {
+            let coun = data[i].name.replace(/[^a-zA-Z]/g, '').toLowerCase();
+            if (coun.includes(countryTosearch.toLowerCase()) && (data[i].region === selectedRegion.value || selectedRegion.value === 'All')) {
+                let hidecard = document.querySelector(`#${coun}`);
+                if (hidecard) {
+                    hidecard.style.display = 'block';
+                }
+            } else {
+                let hidecard = document.querySelector(`#${coun}`);
+                if (hidecard) {
+                    hidecard.style.display = 'none';
+                }
+            }
+        }
+    }
+}
+// filter functionality
+async function filter(data, region) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].region === region) {
+            let coun = data[i].name.replace(/[^a-zA-Z]/g, '').toLowerCase();
+            let hidecard = document.querySelector(`#${coun}`);
+            if (hidecard) {
+                hidecard.style.display = 'block';
+            }
+        } else {
+            let coun = data[i].name.replace(/[^a-zA-Z]/g, '').toLowerCase();
+            let hidecard = document.querySelector(`#${coun}`);
+            if (hidecard) {
+                hidecard.style.display = 'none';
+            }
+        }
+    }
+}
 fetch("../../data.json")
     .then(data => data.json())
     .then(data => {
@@ -122,8 +174,9 @@ fetch("../../data.json")
 
             const card = document.createElement('li');
             card.classList.add('country-card');
-            card.setAttribute('id', `${name_without_spacing}-card`);
-            
+            // card.setAttribute('id', `${name_without_spacing}-card`);
+            card.setAttribute('name', name_without_spacing);
+            card.setAttribute('id', element.name.replace(/[^a-zA-Z]/g, '').toLowerCase());
             card.innerHTML = `
                 <a></a>
                 <img src=${flag_path}>
@@ -136,4 +189,43 @@ fetch("../../data.json")
             await country_page(element, arr, card);
             cards_section.appendChild(card);
         });
+        // calling search
+        searchbtn.onclick = async () => {
+            if (searchinput) {
+                await search(data, searchinput.value);
+            }
+        }
+        // or 
+        searchinput.addEventListener('input', async (e) => {
+            if (e.target.value !== '') {
+                await search(data, searchinput.value);
+            } else {
+                if (selectedRegion.value === 'All') {
+                    returndata();
+                } else {
+                    filter(data, selectedRegion.value);
+                }
+            }
+        });
+        deleteSearch.addEventListener('click', async () => {
+            if (searchinput.value) {
+                searchinput.value = '';
+            }
+        })
+        // calling filter 
+        selectedRegion.addEventListener('change', async (e) => {
+            if (e.target.value === 'All') {
+                returndata();
+            } else {
+                filter(data, e.target.value);
+            }
+            searchinput.value = '';
+        })
     });
+
+async function returndata() {
+    let allcountries = document.querySelectorAll('.country-card');
+    allcountries.forEach((country) => {
+        country.style.display = 'block';
+    })
+}
